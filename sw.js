9722a7ecd5ea116a13617413f5b388eac8c450fa
amd5500gt <jasss.pb03@gmail.com>
2025-10-16 07:55:41 +0530

    yes

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'jwork-v1.3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  // Add other assets you want to cache
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
